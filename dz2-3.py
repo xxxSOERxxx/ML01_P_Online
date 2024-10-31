36e66efa7b49e50fbe96058ef47d0d7f94fe9ea1
xxxSOERxxx <soer.man@gmail.com>
2024-10-31 17:44:00 +0300

    3 задача

---
+++ dz2-3.py
@@ -0,0 +1,27 @@
+import math
+
+print(f"Решаем уравнение типа -26*x**2+25*x-9.")
+a = -26
+b = 25
+c = -9  
+
+print(f"a={a}")
+print(f"b={b}")
+print(f"c={c}")
+x = -5
+R = -26*x**2+25*x-9
+max = R
+min = R
+while x < 5:
+    x = round((x + 0.1),2)
+  #  print(f"x = {x}")
+    R = -26*x**2+25*x-9
+    if R>max:
+        max = R
+     #   print(f"Обнаружен новый максимум: {max}")
+    elif R < min:
+        min = R
+     #   print(f"Обнаружен новый минимум: {min}")
+    continue
+print(f"Максимум функции: {max}")
+print(f"Минимум функции: {min}")
